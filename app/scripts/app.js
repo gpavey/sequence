2393d3a8675a6421823ff2896ac0981fff87d4b1
Geoff Pavey <geoff.pavey@gmail.com>
2015-04-28 15:44:47 -0700

    ran out of time

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,44 @@
     'ui.bootstrap'
   ])
 
+  .controller('ModalCtrl', function ($scope, $modal, $log) {
+    $scope.open = function (size) {
+      var modalInstance = $modal.open({
+        templateUrl: 'myModalContent.html',
+        controller: 'ModalInstanceCtrl',
+        size: size,
+        resolve: {
+          items: function () {
+            return $scope.items;
+          },
+          item: function(){
+            return size;
+          }
+        }
+      });
+    };
+  })
 
+ .controller('ModalInstanceCtrl', function ($scope, $modalInstance, items, item) {
+    $scope.item = item;
+    $scope.items = items;
+    $scope.selected = {
+      item: $scope.items[0]
+    };
+    $scope.increment = function(item){
+      item.count += 1;
+    }
+
+
+
+    $scope.ok = function () {
+      $modalInstance.close($scope.selected.item);
+    };
+
+    $scope.cancel = function () {
+      $modalInstance.dismiss('cancel');
+    };
+  })
 
   .controller('bikeController',function($scope,bikeService){
     $scope.myInterval = 5000;
