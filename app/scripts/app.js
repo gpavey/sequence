8b3b55fde858ea7534dac5339b3008eb97352b1c
Geoff Pavey <geoff.pavey@gmail.com>
2015-04-28 14:00:13 -0700

    added angular

---
+++ app/scripts/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name missionBicycleApp
+ * @description
+ * # missionBicycleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('missionBicycleApp', [
+    'ngSanitize',
+    'ngTouch'
+  ]);
