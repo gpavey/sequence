b4ab0b51e59aa76c92a9c9139d1c18c58a7bd831
Geoff Pavey <geoff.pavey@gmail.com>
2015-04-28 14:34:30 -0700

    got data

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,14 +2,36 @@
 
 /**
  * @ngdoc overview
- * @name missionBicycleApp
+ * @name missionBikeApp
  * @description
- * # missionBicycleApp
+ * # missionBikeApp
  *
  * Main module of the application.
  */
-angular
-  .module('missionBicycleApp', [
+(function(){
+  angular.module('missionBikeApp', [
     'ngSanitize',
-    'ngTouch'
-  ]);
+    'ngTouch',
+    'ui.bootstrap'
+  ])
+
+
+
+  .controller('bikeController',function($scope,bikeService){
+    $scope.myInterval = 5000;
+    $scope.items = {};
+    bikeService.getBikes().then(function(data){
+      $scope.items = data;
+    });
+  })
+
+  .service('bikeService', function($http){
+    this.getBikes = function() {
+    return $http
+      .get('http://seq-front-end-assessment.s3-website-us-west-2.amazonaws.com/catalog.json')
+      .then(function(res) {
+        return res.data;
+      });
+    };
+  });
+})();
